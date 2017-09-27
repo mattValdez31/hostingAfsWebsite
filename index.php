84dcd24a0fa1ab818e7009af7d30ea12423f4a2a
Matthew J Valdez <mjv32@afsaccess1.njit.edu>
2017-09-27 11:29:17 -0400

    added function calls

--- index.php
+++ index.php
@@ -1,5 +1,204 @@
 <?php
+	$m = 'Hi my nAme iS MaTThew';
+	$m2 = "A 'quote' is <b>bold</b>";
 
-	echo "Hello World, another change"
+	$a = array(1,2,3,4,5);
+	$b = array(4,5,6,7,8);
+	$c = array();
+	$d = array(1 => 'mike',2 => 'matt',3 => 'marcus');
 
+	$obj = new main();
+	$obj->asciiToChar(65);
+	$obj->explodeStr(' ', $m);
+	$obj->printThis($m);
+	$obj->stringLength($m);
+	$obj->stringPos($m, 'n');
+	$obj->toUpper($m);
+	$obj->toLower($m);
+	$obj->htmlEnt($m2);
+	$obj->htmlEntDec($m2);
+	$obj->rightTrim($m, 'w');
+
+	$obj->arrayChunk($a,2);
+	$obj->arrayCombine($a,$b);
+	$obj->arrayDiff($a,$b);
+	$obj->arrayDiffKey($a,$b);
+	$obj->arrayFill($c,2,5,9);
+	$obj->arrayFillKeys($b,9);
+	$obj->arrayKeyExists(3,$d);
+	$obj->arrayKeys($d);
+	$obj->arrayPop($a);
+	$obj->arraySearch('matt',$d);
+
+
+	class main
+	{
+		public function __construct()
+		{
+			echo 'Hello i\'m an object';
+		}
+
+		public function asciiToChar($x)
+		{
+			echo '<h1> Ascii to Char function demo </h1>';
+			echo chr($x);
+			echo '<hr>';
+		}
+
+		public function explodeStr($del, $string)
+		{
+			echo '<h1>String explode function demo </h1>';
+			$arrExplode = explode($del, $string);
+			print_r($arrExplode);
+			echo '<hr>';
+		}
+		
+		public function printThis($msg)
+		{
+			echo '<h1>Echo function demo </h1>';
+			echo $msg;
+			echo '<hr>';
+		}
+
+		public function stringLength($msg)
+		{
+			echo '<h1>String length function demo </h1>';
+			$msglen  = strlen($msg);
+			echo $msglen;
+			echo '<hr>';
+		}
+
+		public function stringPos($myString, $findMe)
+		{
+			echo '<h1>String Position function demo </h1>';
+			$pos = strpos($myString, $findMe);
+			echo $pos;
+			echo '<hr>';
+		}
+
+		public function toUpper($msg)
+		{
+			echo '<h1>String to Upper function demo </h1>';
+			$msgU = strtoupper($msg);
+			echo $msgU;
+			echo '<hr>';
+		}
+
+		public function toLower($msg)
+		{
+			echo '<h1>String to Lower function demo </h1>';
+			$msgL = strtolower($msg);
+			echo $msgL;
+			echo '<hr>';
+		}
+		
+		public function htmlEnt($msg3)
+		{
+			echo '<h1>HTML Entities function demo </h1>';
+			$a = htmlentities($msg3);
+			echo $a;
+			echo '<hr>';
+		}
+
+		public function htmlEntDec($msg3)
+		{
+			echo '<h1>HTML Entities Decode function demo </h1>';
+			$a = html_entity_decode($msg3);
+			echo $a;
+			echo '<hr>';
+		}
+		
+		public function rightTrim($msg,$trimStr)
+		{
+			echo '<h1>Right Trim function demo </h1>';
+			$a = rtrim($msg,$trimStr);
+			var_dump($a);
+			echo '<hr>';
+		}
+		
+		//ARRAY FUNCTIONS
+
+		public function arrayChunk($array, $chunkSize)
+		{
+			echo '<h1>Array Chunk function demo </h1>';
+			print_r(array_chunk($array, $chunkSize));
+			echo '<hr>';
+		}
+
+		public function arrayCombine($array1, $array2)
+		{
+			echo '<h1>Array Combine function demo </h1>';
+			print_r(array_combine($array1, $array2));
+			echo '<hr>';
+
+		}
+
+		public function arrayDiff($array1, $array2)
+		{
+			echo '<h1>Array Diff function demo </h1>';
+			print_r(array_diff_assoc($array1, $array2));
+			echo '<hr>';
+		}
+		
+		public function arrayDiffKey($array1, $array2)
+		{
+			echo '<h1>Array Diff Key function demo </h1>';
+			print_r(array_diff_key($array1, $array2));
+			echo '<hr>';
+		}
+
+		public function arrayFill($array, $startInd, $num, $values)
+		{
+			echo '<h1>Array Fill function demo </h1>';
+			$array = array_fill($startInd, $num, $values);
+			print_r($array);
+			echo '<hr>';
+		}
+
+		public function arrayFillKeys($array, $value)
+		{
+			echo '<h1>Array Fill Keys function demo </h1>';
+			$a = array_fill_keys($array, $value);
+			print_r($a);
+			echo '<hr>';
+		}
+		
+		public function arrayKeyExists($key,$array)
+		{
+			echo '<h1>Array Key Exists function demo </h1>';
+			if (array_key_exists($key,$array) == true)
+			{
+				echo $key . ' is in the array';
+			}
+			echo '<hr>';
+		}
+
+		public function arrayKeys($array)
+		{
+			echo '<h1>Array Keys function demo </h1>';
+			print_r(array_keys($array));
+			echo '<hr>';
+		}
+
+		public function arrayPop($array)
+		{
+			echo '<h1>Array Pop function demo </h1>';
+			$a = array_pop($array);
+			print_r($a);
+			echo '<hr>';
+		}
+
+		public function arraySearch($key,$array)
+		{
+			echo '<h1>Array Search function demo </h1>';
+			$x = array_search($key, $array);
+			echo $x;
+			echo '<hr>';
+		}
+		
+		public function __destruct()
+		{
+			echo 'I\'m done </br>';
+		}
+	}
 ?>
